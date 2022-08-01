b3be701c0f808f2b1677c631bb3c75e1361470a9
kuzudoli <enescakkr@windowslive.com>
2022-08-01 13:30:48 +0300

    OCP test

--- day4/Program.cs
+++ day4/Program.cs
@@ -1,42 +1,54 @@
-﻿using day4;
+﻿//using day4;
 
-BL bl = new BL(new DAL());
+//BL bl = new BL(new DAL());
 
-//Events
-bl.DownThresholdEvent += Bl_DownThresholdEvent;
+////Events
+//bl.DownThresholdEvent += Bl_DownThresholdEvent;
 
-void Bl_DownThresholdEvent(int count)
+//void Bl_DownThresholdEvent(int count)
+//{
+//    Console.WriteLine("Ürün sayısı kritik durumda: " + count);
+
+//}
+
+//bl.UpThresholdEvent += Bl_UpThresholdEvent;
+
+//void Bl_UpThresholdEvent(int count)
+//{
+//    Console.WriteLine("Maksimum ürün sayısına ulaşıldı: " + count);
+//}
+
+//bl.AddProduct(new() { id = 0, name = "Ürün 1", price = 100 });
+//bl.AddProduct(new() { id = 1, name = "Ürün 2", price = 100 });
+//bl.AddProduct(new() { id = 2, name = "Ürün 3", price = 100 });
+//bl.AddProduct(new() { id = 3, name = "Ürün 4", price = 100 });
+//bl.AddProduct(new() { id = 4, name = "Ürün 5", price = 100 });
+//bl.AddProduct(new() { id = 5, name = "Ürün 6", price = 100 });
+//bl.AddProduct(new() { id = 6, name = "Ürün 7", price = 100 });
+//bl.AddProduct(new() { id = 7, name = "Ürün 8", price = 100 });
+//bl.AddProduct(new() { id = 8, name = "Ürün 9", price = 100 });
+//bl.AddProduct(new() { id = 9, name = "Ürün 10", price = 100 });
+//bl.AddProduct(new() { id = 10, name = "Ürün 11", price = 100 });
+
+
+//bl.ReadProductList().ForEach(x =>
+//{
+//    Console.WriteLine(x.name);
+//});
+
+//for(var i=0;i<7; i++)
+//{
+//    bl.RemoveProduct(i);
+//}
+
+
+//SOLID
+//->OCP
+using day4.SOLID;
+
+ProductRepository2 repository2 = new ProductRepository2(new MongoDBRepository());
+
+repository2.GetAll().ForEach(x =>
 {
-    Console.WriteLine("Ürün sayısı kritik durumda: " + count);
-
-}
-
-bl.UpThresholdEvent += Bl_UpThresholdEvent;
-
-void Bl_UpThresholdEvent(int count)
-{
-    Console.WriteLine("Maksimum ürün sayısına ulaşıldı: " + count);
-}
-
-bl.AddProduct(new() { id = 0, name = "Ürün 1", price = 100 });
-bl.AddProduct(new() { id = 1, name = "Ürün 2", price = 100 });
-bl.AddProduct(new() { id = 2, name = "Ürün 3", price = 100 });
-bl.AddProduct(new() { id = 3, name = "Ürün 4", price = 100 });
-bl.AddProduct(new() { id = 4, name = "Ürün 5", price = 100 });
-bl.AddProduct(new() { id = 5, name = "Ürün 6", price = 100 });
-bl.AddProduct(new() { id = 6, name = "Ürün 7", price = 100 });
-bl.AddProduct(new() { id = 7, name = "Ürün 8", price = 100 });
-bl.AddProduct(new() { id = 8, name = "Ürün 9", price = 100 });
-bl.AddProduct(new() { id = 9, name = "Ürün 10", price = 100 });
-bl.AddProduct(new() { id = 10, name = "Ürün 11", price = 100 });
-
-
-bl.ReadProductList().ForEach(x =>
-{
-    Console.WriteLine(x.name);
+    Console.WriteLine(x);
 });
-
-for(var i=0;i<7; i++)
-{
-    bl.RemoveProduct(i);
-}
