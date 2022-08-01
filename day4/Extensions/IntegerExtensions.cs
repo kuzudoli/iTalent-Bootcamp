77c7888b62a61c0f4b7d9810315c98482e16ed35
kuzudoli <enescakkr@windowslive.com>
2022-08-01 15:43:32 +0300

    create integer extension

---
+++ day4/Extensions/IntegerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4.Extensions
+{
+    internal static class IntegerExtensions
+    {
+        internal static int KareAl(this int number)
+        {
+            return (int)Math.Pow((double)number,2);
+        }
+    }
+}
