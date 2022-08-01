40f6c992f24a313f40e479f44e37c50876a00a8f
kuzudoli <enescakkr@windowslive.com>
2022-08-01 13:30:14 +0300

    OCP

---
+++ day4/SOLID/OCP.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4.SOLID
+{
+    //Object Closed Principle
+    public interface IRepository
+    {
+        List<string> GetAll();
+    }
+    public class SqlRepository : IRepository
+    {
+        public List<string> GetAll()
+        {
+            return new List<string>() { "SQL DATA 1","SQL DATA 2"}; 
+        }
+    }
+    public class MongoDBRepository : IRepository
+    {
+        public List<string> GetAll()
+        {
+            return new List<string>() { "MONGODB DATA 1", "MONGODB DATA 2" };
+        }
+    }
+    internal class ProductRepository2
+    {
+        private readonly IRepository repository;
+
+        public ProductRepository2(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public List<string> GetAll()
+        {
+            return repository.GetAll();
+        }
+    }
+}
