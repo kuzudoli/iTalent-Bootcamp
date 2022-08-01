3ee1eafed5a065bfb2d6230ee6d1adbb053ea816
kuzudoli <enescakkr@windowslive.com>
2022-08-01 15:43:58 +0300

    create extension for dto

---
+++ day4/Extensions/DtoExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4.Extensions
+{
+    internal static class DtoExtensions
+    {
+        internal static List<PersonDto> toConvertDto(this IEnumerable<Person> list)
+        {
+            return list.Select(x => new PersonDto() { fullname = $"{x.name}-{x.surname}", age = x.age }).ToList();
+        }
+    }
+}
---
+++ day4/Extensions/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4.Extensions
+{
+    internal class Person
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int age { get; set; }
+
+    }
+
+    internal class PersonDto
+    {
+        public string fullname { get; set; }
+        public int age { get; set; }
+
+    }
+}
